bbdc3d5471f3dd8f631dca95b9b2608e39d35351
Rene Vallecillo Vega <reneval@gmail.com>
2018-10-04 20:58:30 -0600

    Adding Search Feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { SearchComponent } from './feature/search/search.component';
+import { SearchModule } from './feature/search/search.module';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, CoreModule, SharedModule],
+  declarations: [AppComponent, SearchComponent],
+  imports: [BrowserModule, CoreModule, SharedModule, SearchModule],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/feature/search/search.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SearchRoutingModule } from './search-routing.module';
+
+@NgModule({
+  imports: [CommonModule, SearchRoutingModule],
+  declarations: []
+})
+export class SearchModule {}
