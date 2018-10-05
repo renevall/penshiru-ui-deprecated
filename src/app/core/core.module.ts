47b402e08ee35efd3147534d735b19991134a426
Rene Vallecillo Vega <reneval@gmail.com>
2018-10-04 20:45:38 -0600

    Adding flex to core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, FlexLayoutModule],
   declarations: []
 })
 export class CoreModule {}
